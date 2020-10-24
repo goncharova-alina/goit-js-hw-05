d4bd4313f8d36da7460a77a5d6e2d8013e653fc9
Alina Goncharova <goncarovaalina206@gmail.com>
2020-10-24 16:27:14 +0300

    task-01,02,03

--- js/task-03.js
+++ js/task-03.js
@@ -1,44 +1,69 @@
-const add = (accum, element) => accum + element;
-const mult = (accum, element) => accum * element;
-const sub = (accum, element) => accum - element;
-
-function reduceArray(array, cb, initial) {
-  'use strict';
-  let i;
-  let accum;
-  if(arguments.length >= 3) {
-    accum = initial;
-    i = 0;
+class Storage {
+   constructor(item) {
+    this.item = item;
+   }
+   getItems(){
+    return this.item
+   }
+   addItem(item){
+    return this.item.push(item)
+   }
+   removeItem(item){
+    for (let i = 0; i < item.length; i += 1) {
+      let i = this.item.indexOf(item);
+      return this.item.splice(i, 1);
+      }
+    }
   }
-  if(arguments.length === 2) {
-    accum = array[0];
-    i = 1;
-  }
-  for(i; i < array.length; i += 1) {
-    const element = array[i];
-    // Write code under this line
-    accum = (cb(accum, element));
-  }
-  return accum;
-}
+ 
+ console.log(typeof Storage);
+// 'function'
 
-const arr  = [1,2,3,4,5];
+const goods = [
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор'
+];
 
-console.log(reduceArray(arr, add)); // 15
-console.log(reduceArray(arr, add, 10)); // 25
+ const storage = new Storage(goods);
 
-console.log(reduceArray(arr, mult)); // 120
-console.log(reduceArray(arr, mult, 10)); // 1200
+ console.log(storage.getItems());
+/* [
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор'
+] */
 
-console.log(reduceArray(arr, sub)); // -13
-console.log(reduceArray(arr, sub, 10)); // -5
+storage.addItem('Дроид');
+console.log(storage.getItems());
+/* [
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор',
+  'Дроид'
+] */
+
+storage.removeItem('Пролонгер');
+ console.log(storage.getItems());
+/* [
+  'Нанитоиды',
+  'Железные жупи',
+  'Антигравитатор',
+  'Дроид'
+] */
 
 
-// Callback функция для получения одного вычисляемого значения массива
-// Функции add, sub и mult принимают два параметра - accum и element , 
-// возвращает число - сумму, разность или произведение параметров.
 
-// Дополни тело функции reduceArray строкой присвоения accum вызова функции cb. 
-// Функция reduceArray должна будет подсчитать сумму или разность или 
-// произведение всех элементов массива в зависимости от того какая именно 
-// из трех функция (add, mult, sub) будет передана в качестве cb.
+// использование методов класса
+// Напиши класс Storage, который будет создавать объекты для управления складом товаров. 
+// При вызове будет получать один аргумент - начальный массив товаров, 
+// и записывать его в свойство items.
+
+// Добавь методы класса:
+
+// getItems() - возвращает массив текущих товаров
+// addItem(item) - получает новый товар и добавляет его к текущим
+// removeItem(item) - получает товар и, если он есть, удаляет его из текущих
--- js/task-04.js
+++ js/task-04.js
@@ -1,39 +1,29 @@
-const account = {
-  owner: 'Mango',
-  balance: 24000,
-  discount: 0.1,
-  orders: ['order-1', 'order-2', 'order-3'],
-  changeDiscount(value) {
-    this.discount = value; // Write code in this line
-  },
-  showOrders() {
-    return this.orders; // Write code in this line
-  },
-  addOrder(cost, order) {
-    this.balance -= cost; // Write code in this line
-    this.orders.push(order); // Write code in this line
-  },
-};
-const copyAccount =  Object.assign({},account);
-copyAccount.orders = [...account.orders];
-// копируем для автотестов ссылочные типы
+ // Write code under this line
+  
 
+ console.log(typeof StringBuilder);
+ // 'function'
+ 
+ 
+ const builder = new StringBuilder('.');
+ 
+ builder.append('^');
+ console.log(builder.value); // '.^'
+ 
+ builder.prepend('^');
+ console.log(builder.value); // '^.^'
+ 
+ builder.pad('=');
+ console.log(builder.value); // '=^.^='
 
-account.changeDiscount(0.15);
-console.log(account.discount); // 0.15
+// переиспользование методов класса
+// Напиши класс StringBuilder. На вход он получает один параметр - строку, 
+// которую записывает в свойство _value.
 
-console.log(account.showOrders()); 
-// ['order-1', 'order-2', 'order-3']
+// Добавь классу следующий функционал:
 
-account.addOrder(5000, 'order-4');
-console.log(account.balance); // 19000
-
-console.log(account.showOrders());
-// ['order-1', 'order-2', 'order-3', 'order-4']
-
-
-
-// this в методах объекта
-// Расставь отсутствующие this в методах объекта account.
-
-// В комментариях показаны операции с объектом и ожидаемые результаты.
+// Геттер value - возвращает текущее значение поля _value
+// Метод append(str) - получает параметр str (строку) и добавляет ее в конец _value
+// Метод prepend(str) - получает параметр str (строку) и добавляет ее в начало value
+// Метод pad(str) - получает параметр str (строку) и добавляет ее в начало и в конец _value
+// Метод pad должен использовать методы append и prepend
